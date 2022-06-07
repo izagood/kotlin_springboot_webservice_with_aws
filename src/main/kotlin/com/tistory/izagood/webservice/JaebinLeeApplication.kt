146af4ad8f3ba8a1e035c2c22ca04c2a2f33b4ae
izagood <ljbfif50@gmail.com>
2022-06-07 21:00:10 +0900

    project init

---
+++ src/main/kotlin/com/tistory/izagood/webservice/JaebinLeeApplication.kt
@@ -0,0 +1,11 @@
+package com.tistory.izagood.webservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JaebinLeeApplication
+
+fun main(args: Array<String>) {
+	runApplication<JaebinLeeApplication>(*args)
+}
---
+++ src/test/kotlin/com/tistory/izagood/webservice/JaebinLeeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tistory.izagood.webservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JaebinLeeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
